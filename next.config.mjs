e20bb15d4c52b32c2a031a8e95518dc59a6a10f1
thisislokeshrm <113114297+thisislokeshrm@users.noreply.github.com>
2024-10-13 17:16:27 +0530

    blabba

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+// next.config.js
+
+module.exports = {
+    reactStrictMode: true,
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+}
+
+
 export default nextConfig;
